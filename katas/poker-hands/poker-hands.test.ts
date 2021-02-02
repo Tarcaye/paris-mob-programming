b894d4212830c1dddb7c51b772c29b86944f8c77
pinage404 <pinage404@gmail.com>
2021-02-02 21:16:42 +0100

    mob next [ci-skip]

--- katas/poker-hands/poker-hands.test.ts
+++ katas/poker-hands/poker-hands.test.ts
@@ -1,17 +1,14 @@
-test("Fake poker hands test", () => {
-	expect(true).toBe(true);
-});
-
-
-function isTie(first?: string, second?: string) {
-	if (second === "2D") return false
-	return true;
+function isTie(first: string, second: string) {
+  if (second === "2D") return false;
+  return true;
 }
 
-test("on donne 2 cartes on dit que c'est égal", () => {
-	expect(isTie("AS","AD")).toBe(true);
-})
+describe("", () => {
+  test("tie for 2 cards with the same value", () => {
+    expect(isTie("AS", "AD")).toBe(true);
+  });
 
-test("2on donne 2 cartes on dit que c'est égal", () => {
-	expect(isTie("AS","2D")).toBe(false);
-})
+  test("", () => {
+    expect(isTie("AS", "2D")).toBe(false);
+  });
+});
