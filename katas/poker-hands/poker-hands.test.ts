66f3e14b7f6310ac0200f6c4fa476cfd6cf14a9f
Abdessamed Mansouri <Abdessamed.mansouri@lacombedulionvert.fr>
2021-02-02 20:41:20 +0100

    mob next [ci-skip]

--- katas/poker-hands/poker-hands.test.ts
+++ katas/poker-hands/poker-hands.test.ts
@@ -2,6 +2,7 @@ test("Fake poker hands test", () => {
 	expect(true).toBe(true);
 });
 
+
 test("on donne 2 cartes on dit que c'est égal", () => {
-	expect(toto("2C", "2D")).toBe(true)
+	expect(tipi()).toBe(true);
 })
