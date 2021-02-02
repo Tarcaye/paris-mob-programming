d55e7ce8f2645589b65041ec798695fe6100575f
hadrienmp <public@hadrienmp.fr>
2021-02-02 21:32:01 +0100

    mob next [ci-skip]

--- katas/poker-hands/poker-hands.test.ts
+++ katas/poker-hands/poker-hands.test.ts
@@ -1,19 +1,25 @@
 function isTie(first: string, second: string) {
-  if(first[2]) return false;
-  return first[0] === second[0];
+    if (first.length >= 3) {
+        return first[3] === second[3];
+    }
+    return first[0] === second[0];
 }
 
 describe("tie", () => {
-  test("yes for 2 cards with the same value", () => {
-    expect(isTie("AS", "AD")).toBe(true);
-  });
+    test("yes for 2 cards with the same value", () => {
+        expect(isTie("AS", "AD")).toBe(true);
+    });
 
-  test("no for 2 cards with different values", () => {
-    expect(isTie("AS", "2D")).toBe(false);
-  });
+    test("no for 2 cards with different values", () => {
+        expect(isTie("AS", "2D")).toBe(false);
+    });
 
-  test("2no for 2 cards with different values", () => {
-    expect(isTie("AS AD", "AH 2S")).toBe(false);
-  });
+    test("2no for 2 cards with different values", () => {
+        expect(isTie("AS AD", "AH 2S")).toBe(false);
+    });
+
+    test("2no for 2 cards with different values", () => {
+        expect(isTie("AS AD", "AH AC")).toBe(true);
+    });
 
 });
